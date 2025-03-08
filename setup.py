c3aa42b6790ed7764358f435cbb2c7b802b93a22
DADA NANJESHA <39907623+DadaNanjesha@users.noreply.github.com>
2025-03-08 22:13:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as fh:
 
 setuptools.setup(
     name="TagGenerator",
-    version="1.0.0",
+    version="1.0.1",
     author="Your Name",
     author_email="your.email@example.com",
     description="A reusable Django app for QR Code generation",
