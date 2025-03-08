79559d3c7806dfac18565e4c8e8ae21c3b470cad
DADA NANJESHA <dadananjesha.rymec@gmail.com>
2025-03-08 15:53:07 +0100

    updated project

--- QrCode/models.py
+++ QrCode/models.py
@@ -1,16 +1,3 @@
 from django.db import models
 
-
 # Create your models here.
-class Gtin(models.Model):
-    gtin_number = models.CharField(max_length=30)
-    product_name = models.CharField(max_length=30)
-    create_date = models.DateTimeField(auto_now_add=True)
-    update_date = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.product_name
-
-    class Meta:
-        verbose_name_plural = "GTIN NUMBER"
-        verbose_name = "GTIN NUMBER"
--- QrCode/views.py
+++ QrCode/views.py
@@ -1,13 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
-from django.template.context_processors import request
-
-
-def my_app(request):
-    return render(request, 'home.html')
-
-
-def view_article(request, article_id):
-    text = "Displaying article Number : %s" % article_id
-    return HttpResponse(text)
